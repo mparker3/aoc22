0b803a8d8be901ed0848495049ee0ed30e61bd2f
Matt Parker <matt.parker@hinge.co>
2022-12-01 14:30:43 -0500

    initial commit

---
+++ 1/part1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+set -eo pipefail
+
+inputfile=$1
+if [[ ! -f $inputfile ]]; then
+	echo "need valid input file"
+fi;
+
+sums=()
+curr_sum=0
+while read line;
+do
+	if [[ -z $line ]];
+	then
+	  sums+=($curr_sum)
+	  curr_sum=0
+	else
+	  ((curr_sum+=$line))
+	fi
+done <"$inputfile"
+# add whatever the last element is, because there won't be a final newln
+sums+=($curr_sum)
+printf "%d\n" "${sums[@]}" | sort | tail -n 1
+
+
