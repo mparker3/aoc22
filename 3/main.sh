60543f316b67052070953760c19d84bde10c63de
Matt Parker <matt.parker@hinge.co>
2022-12-03 13:05:20 -0500

    part 2

--- 3/main.sh
+++ 3/main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-while read -r line; do
-	# split lines into first and second halves
-	first=$(echo $line | awk '{ half = length($1)/2; print substr($1, 0, half)}')
-	second=$(echo $line | awk '{ half = length($1)/2; print substr($1, half + 1)}')
+while read -r line1 && read -r line2 && read -r line3; do
 	
-	# `fold` each half into a newline-delimited file, sort them for use with comm, find the common characters, and then cut to only the first incidence of each character
-	char=$(echo $(comm -12 <(echo $first | fold -w1 | sort) <(echo $second | fold -w1 | sort)) | cut -c1)
+	# `fold` both of the first two lines into a newline-delimited files sort them for use with comm and find the common characters between the first and second lines
+	oneandtwo=$(echo $(comm -12 <(echo $line1 | fold -w1 | sort) <(echo $line2 | fold -w1 | sort)))
+
+	char=$(echo $(comm -12 <(echo $oneandtwo | fold -w1 | sort) <(echo $line3 | fold -w1 | sort)))
+
 	# convert the char to its ASCII value
 	value=$(printf '%d' "'$char")
 	# convert the ascii value to its priority
@@ -18,4 +18,4 @@ while read -r line; do
 	fi
 
 	echo $prio
-done < input.txt | awk '{ sum+=$1 ; print sum } END { print sum }'
+done < input.txt | awk '{ sum+=$1  } END { print sum }'
