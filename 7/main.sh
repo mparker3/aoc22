9bb71b746a12ea7a9afaa4498d3b338a8276dab5
Matt Parker <matt.parker@hinge.co>
2022-12-07 11:52:58 -0500

    day 7 part 2

--- 7/main.sh
+++ 7/main.sh
@@ -27,6 +27,24 @@ BEGIN { ctr=0; parent="root"; curr="root"}
 END { for (i in sums) print sums[i] } '
 
 # some preprocessing required here, POSIX standard specifies a newline at the end of a file, so running this _without_ adding that newline causes the last line to be left out. TODO(mparker): fix this
+
+sorted_sizes=$(mktemp /tmp/aoc-7.txt)
+
+# set a read and write FD we can use to read and write from the tempfile, but immediately delete the directory entry.
+# When we close the FD at the end of the script, this will dealloc the memory actually allocated to the file. 
+# https://unix.stackexchange.com/questions/181937/how-create-a-temporary-file-in-shell-script
+exec 3>"$sorted_sizes"
+exec 4<"$sorted_sizes"
+rm "$sorted_sizes"
+
 while read line; do
 	echo -e $line
-done < input.txt | awk "$CMD" | awk '{ if ($1 <= 100000) { sum+=$1 } } END { print sum }'
+done < input.txt | awk "$CMD" | sort --reverse --numeric-sort >&3
+fs_disk=70000000
+need=30000000
+used=$(head -n 1 <&4)
+
+tail -n +2 <&4 | awk -v used="$used" -v need="$need" -v fs_disk="$fs_disk" '{ if ( (fs_disk - used + $1) >= need ) print $1 }' | tail -n 1
+
+#remove sorted_sizes fd
+exec 3>&-
